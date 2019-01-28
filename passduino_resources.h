abbe0153398ee5e8d7447fd1f23a89b45071fd39
Mike Teehan <mteehan@tocr.com>
2019-01-28 00:05:18 -0500

    Correct the <title>

--- passduino_resources.h
+++ passduino_resources.h
@@ -6,7 +6,7 @@ const char PAGE_HTML[] = R"=====(
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>WiFi Robot</title>
+    <title>Passduino</title>
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -31,11 +31,11 @@ const char PAGE_HTML[] = R"=====(
 function trigger(macro) {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function() { return; };
-  xmlHttp.open("GET", "/macro?" + macro, true); // true for asynchronous 
+  xmlHttp.open("GET", "/macro?" + macro, true); // true for asynchronous
   xmlHttp.send(null);
 }
     </script>
-  
+
     <!-- Latest compiled and minified JavaScript -->
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
